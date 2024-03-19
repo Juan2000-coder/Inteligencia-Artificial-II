71dbc1270b9e008d25770cd07dcbe96f81dd1ef0
Juan <borquez.juan00@gmail.com>
2024-03-19 19:16:43 -0300

    RECOGIDOOOO!!!!

--- TP1/Ejercicio3.py
+++ TP1/Ejercicio3.py
@@ -13,35 +13,15 @@ if __name__ == '__main__':
 
     enviroment           = Enviroment(shelves_rows, shelves_columns)
 
-    game                = Game(enviroment)
-    start_pos, goals_pos = game.get_checkpoints_list()
+    game                 = Game(enviroment)
+    start_pos, goals_pos = game.get_checkpoints_list() 
 
+    recocido = rc(200, 0.5, 50, enviroment)
+    solucion_optima, camino_optimo = recocido.ejecutar_recocido(goals_pos)
 
-    """
-    start = start_pos.pop()
-
-    recocido = rc(enviroment)
-    mejor_solucion = recocido.ejecutar_recocido()
-    print(mejor_solucion)
-    """
-    for i, goal in enumerate(goals_pos):
-        if i == 0:
-            problem = Problem(enviroment, start_pos[0], goal)
-            agent   = Agent(problem)
-        else: 
-            agent.problem.start = agent.path[-1]
-            agent.problem.goal  = goal
-            agent.a_star.re_init(agent.problem)
-        agent.path       += agent.a_star.solve()
-        #enviroment.ocupied.append(agent.position)
-        '''print("start_pos: ", start_pos)
-        print("goal_pos: " , goals_pos)
-        print(f"Trayecto {i+1}: ", agent.path)'''
-        
-        costo_trayecto = len(agent.path)
-        print(f"Costo actual: ", costo_trayecto)
-        #start_pos[0] = goals_pos[i]
+    problema = Problem(enviroment, (0, 0), goals_pos[-1])
+    agent = Agent(problema)
+    agent.path = camino_optimo
 
     enviroment.ocupied.append(agent.position)
-    # Chequear para que funque bien
     game.run_ej3(agent)
--- TP1/Recocido.py
+++ TP1/Recocido.py
@@ -3,62 +3,83 @@ import random
 from Problem import Problem
 from Astar import A_star
 from Enviroment import Enviroment
+import copy
 
 class Recocido:
+	def __init__(self, T, T_min, L, enviroment:Enviroment):
+		self.T = T
+		self.T_min = T_min
+		self.L = L     #math.factorial(len(self.estado_inicial)) // 2
+		self.enviroment = enviroment
 
-    def __init__(self,_enviroment:Enviroment):
-        self.T = 200
-        self.T_min = 0.5
-        self.estado_inicial = [22 , 30, 1, 4]
-        self.L = math.factorial(len(self.estado_inicial))//2
-        self.entorno = _enviroment
-        self.pb = Problem(self.entorno,(0,0),(1,1))
-        self.estrella = A_star(self.pb)
-
-    def esquema_enfriamiento(self, temperatura):
-        return temperatura * 0.2
-
+    # Funcion variación de la temperatura
+	def esquema_enfriamiento(self, temperatura):
+		return temperatura * 0.2
+	
     # Función para generar una solución vecina (perturbación)
-    def generar_vecino(self, solucion_actual):
-        return random.shuffle(solucion_actual)
-    def energia(self ,estado):
-        self.pb.start = (0,0)
-        self.pb.goal = self.entorno.shelf2coor(estado[0])
-        self.estrella.re_init(self.pb)
-        E = len(self.estrella.solve())
-        
-        for i in range(len(estado)-1):
-            self.pb.start = self.entorno.shelf2coor(estado[i])
-            self.pb.goal = self.entorno.shelf2coor(estado[i+1])
-            self.estrella.re_init(self.pb)
-            E = E + len(self.estrella.solve())
-
-        return E
+	def generar_vecino(self, solucion_actual):
+		vecino = copy.deepcopy(solucion_actual)
+		random.shuffle(vecino)
+		return vecino
+	
+	def energia(self, estado):
+		path:list = []
+		start_pos = (0, 0)
+		# Calculo del camino completo de ida
+		for i, goal in enumerate(estado):
+				if i == 0:
+					# Problema inicial desde la posición de inicio al primer objetivo
+						#problem  = Problem(self.enviroment, start_pos, self.enviroment.shelf2coor(goal))
+						problem  = Problem(self.enviroment, start_pos, goal)
+						a_star   = A_star(problem)
+				else:
+					# Redefine el problema por cada objetivo
+						a_star.problem.start = path[-1]
+						#a_star.problem.goal  = self.enviroment.shelf2coor(goal)
+						a_star.problem.goal  = goal
+						a_star.re_init(a_star.problem)
+				# Actualiza el camino
+				path       += a_star.solve()
+		
+		#-------APPENDEA EL CAMINO DE VUELTA-----------#
+		a_star.problem.start = path[-1]
+		a_star.problem.goal  = start_pos
+		a_star.re_init(a_star.problem)
+		path       	+= a_star.solve()
+		#----------------------------------------------#
+		return len(path), path
+	
     # Algoritmo de recocido simulado
-    def ejecutar_recocido(self):
-        solucion_actual = self.estado_inicial  # Solución inicial aleatoria
-        temperatura = self.T
-        energia_actual = self.energia(solucion_actual) 
+	def ejecutar_recocido(self, solucion_inicial: list):
+		solucion_actual = solucion_inicial
+		temperatura = self.T
+		energia_actual, camino_actual = self.energia(solucion_actual)
 
-        while temperatura>self.T_min:
-            for i in range(self.L):
-                vecino = self.generar_vecino(solucion_actual)
-                energia_vecino = self.energia(vecino)
-                d_E = energia_actual-energia_vecino
+		while temperatura > self.T_min:
+			for i in range(self.L):
+				vecino = self.generar_vecino(solucion_actual)
+				energia_vecino, camino_vecino = self.energia(vecino)
+				d_E = energia_actual - energia_vecino
+				
+				if d_E > 0:
+					solucion_actual = vecino
+					camino_actual = camino_vecino
+					energia_actual  = energia_vecino
+				elif random.random() < math.exp(d_E / temperatura):
+					solucion_actual = vecino
+					camino_actual 	= camino_vecino
+					energia_actual 	= energia_vecino
+			temperatura = self.esquema_enfriamiento(temperatura)
+		return solucion_actual, camino_actual
 
-                if random.random()<math.exp(d_E/temperatura):
-                    solucion_actual = vecino
-            temperatura = self.esquema_enfriamiento(temperatura)
-        return solucion_actual
+  # Parámetros del algoritmo
+  #temperatura_inicial = 100
+  #enfriamiento = 0.95
+  #iteraciones = 1000
+  #rango_perturbacion = 0.1
 
-    # Parámetros del algoritmo
-    #temperatura_inicial = 100
-    #enfriamiento = 0.95
-    #iteraciones = 1000
-    #rango_perturbacion = 0.1
+  # Ejecutar el algoritmo
+  #mejor_solucion, valor_objetivo = self.recocido_simulado(funcion_objetivo, temperatura_inicial, enfriamiento, iteraciones, rango_perturbacion)
 
-    # Ejecutar el algoritmo
-    #mejor_solucion, valor_objetivo = self.recocido_simulado(funcion_objetivo, temperatura_inicial, enfriamiento, iteraciones, rango_perturbacion)
-
-    #print("Mejor solución encontrada:", mejor_solucion)
-    #print("Valor objetivo de la mejor solución:", valor_objetivo)
+  #print("Mejor solución encontrada:", mejor_solucion)
+  #print("Valor objetivo de la mejor solución:", valor_objetivo)

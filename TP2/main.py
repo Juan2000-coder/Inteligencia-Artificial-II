84779359047aab9441131652a7775bad694a1a83
Juan Manuel BORQUEZ PEREZ <118025372+Juan2000-coder@users.noreply.github.com>
2024-04-24 13:46:51 +0000

    Algunas aclaraciones

--- TP2/main.py
+++ TP2/main.py
@@ -7,13 +7,17 @@ if __name__ == '__main__':
 	diaCompleto = [x * 0.5 for x in range(0, int(24 / 0.5) + 1)]
 	
 	#Terminos linguisticos de Z
-	ZN = HombroDerecho(0, 50)   # Hay que ver en que valores varia
-	ZC = Triangular(0, 50)			# Hay que ver en qué valores varía
-	ZP = HombroIzquierdo(0, 50) # Hay que ver en que valores varía
+	ZN = HombroDerecho(0, 50)   # Hay que ver en que valores varia (Znegativo)
+	ZC = Triangular(0, 50)			# Hay que ver en qué valores varía	(Zcentro)
+	ZP = HombroIzquierdo(0, 50) # Hay que ver en que valores varía	(Zpositivo)
 
+	#Terminos linguisticos de V (ventana)
+	VA	= HombroIzquierdo(50, 20) # Ventana abrir
+	VC	= HombroDerecho(50, 20)		# Ventana cerrar
+	VM	= Triangular(50, 20)			# Ventana medio
+	
 	#-----------------------Definición de variables linguísticas-----------------------
 	Z = VariableLinguistica('Z', {'ZN':ZN, 'ZC':ZC, 'ZP':ZP}, [-1000, 1000]) # Ajustar los valores despues
-
   # Zenf
   # Zcal
   # Hora
@@ -21,7 +25,8 @@ if __name__ == '__main__':
 	# Tp
 
 	VariablesBorrosas = [Z, ZCal, ZEnf, TP, Hora] # Colocamos todas las variables en una lista
-
+	V									= VariableLinguistica('V', {'VA':VA, 'VC':VC, 'VM':VM}, [0, 100])
+	
 	#--------------------------------Variables nitidas----------------------------------
 	ZNitida 		= None
 	ZCalNitida 	= None
@@ -31,11 +36,11 @@ if __name__ == '__main__':
 	VariablesNitidas	 = [Znitida, ZCalNitida, ZEnfNitida, TPNitida, HoraNitida]
 
 	#----------------------Iteración en el paso de tiempo--------------------------------
+	VectorTemperaturaAmbiente						# Serie de temperatura exterior
+	VectorTiempos												# Serie de tiempos en correspondiente con la Tambiente
 	while(True):
-		VectorTemperaturaAmbiente						# Serie de temperatura exterior
-		
 		#-------------------------------Medición------------------------
-		Hora_n 	= 																	# Hora actual
+		Hora_n 			= VectorTiempos.pop()								# Hora actual
 		TENitida 		= VectorTemperaturaAmbiente.pop()		# Temperatua exterior actual
 		TINitida		=																		# La temperatura interior actual
 		TONitida		= 25																# La temperatura objetivo actual
@@ -55,10 +60,26 @@ if __name__ == '__main__':
 		# Evaluar todos los terminos linguisticos de todas las variables
 		ValoresBorrosos = {}
 		for VariableNitida, VariableBorrosa in zip(VariablesNitidas, VariablesBorrosas):
+			Terminos = {}
 			for Termino in VariableBorrosa.tling:
-				# En donde verga guardamos los valores?
-				ValoresBorrosos[VariableBorrosa.Nombre][Termino] = VariableBorrosa.es(Termino, VariableNitida)
+				Terminos[Termino] = VariableBorrosa.es(Termino, VariableNitida)
+			ValoresBorrosos[VariableBorrosa.Nombre] = dict(Terminos)
 				
 		#--------------------Evaluar las reglas de la base de conocimiento---------------------
-		f1 = Then(And(ValoresBorrosos['H']['D'], ValoresBorrosos['Z']['ZP']), ValoresBorrosos['V']['A'])
-		# Se obtiene como resultado una función de pertenencia también para cada regla
+		# Se obtiene como resultado para cada regla, una función de pertenencia
+		f1 = Then(And(ValoresBorrosos['H']['D'], ValoresBorrosos['Z']['ZP']), V.tling['VC'])
+		# f2
+		# f3
+		# f4
+		# f5
+		# f6
+		# f7
+		# f8
+		# f9
+		
+		# Se juntan todas las funciones de pertenencia
+		f = Or(f1, f2, f3, f4, f5, f6, f7, f8, f9)
+
+		#--------------------------------Desborrosificacion-------------------------------------
+		# Se obtiene el centroide a partir de f que estará definida en un subintervalo del rango de la ventana
+		# que es de 0 a 100

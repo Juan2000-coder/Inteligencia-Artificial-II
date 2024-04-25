34e23d2af8ef04c0e0ea87faa1fa41b449bb72f3
Juan200-coder <borquez.juan00@gmail.com>
2024-04-25 05:57:27 -0700

    incorporacion de la Or

---
+++ TP2/Operadores.py
@@ -0,0 +1,12 @@
+from FuncionesPertenencia import FuncionPertenencia
+
+class Or(FuncionPertenencia):
+	def __init__(self, *args):
+		super().__init__()
+		self.Operandos = args
+    
+       
+	def Evaluar(self, *args):
+		ValoresBorrosos = [Operando.Evaluar(arg) for arg, Operando in zip(args, self.Operandos)]
+		return max(ValoresBorrosos, default = 0)
+    

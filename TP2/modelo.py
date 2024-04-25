5730a0ed679e9d4f0644ed30e4952dbd05bce7bb
Valentin Panonto <127955872+valentinpanonto@users.noreply.github.com>
2024-04-25 15:50:52 +0000

    agregado plots

--- TP2/modelo.py
+++ TP2/modelo.py
@@ -12,14 +12,14 @@ class Habitacion:
       """
       return (_tempE - _tempI)/(self.tau*(1.1 - (0.1*_porcentaje/100)))
 
-  def runge_kutta_4(x, y, h):
+  def runge_kutta_4(self, _tA, _tempIA, h,_tempE, _porcentaje):
       """
       Método de Runge-Kutta de cuarto orden para resolver EDOs.
       """
-      k1 = h * self.f(x, y)
-      k2 = h * self.f(x + h/2, y + k1/2)
-      k3 = h * self.f(x + h/2, y + k2/2)
-      k4 = h * self.f(x + h, y + k3)
+      k1 = h * self.f(x, y, _tempE, _porcentaje)
+      k2 = h * self.f(x + h/2, y + k1/2, _tempE, _porcentaje)
+      k3 = h * self.f(x + h/2, y + k2/2, _tempE, _porcentaje)
+      k4 = h * self.f(x + h, y + k3, _tempE, _porcentaje)
       y_next = y + (k1 + 2*k2 + 2*k3 + k4) / 6
       return y_next
 

78455f3005d8658fa4e55b32ea0ec3ed00545326
Miranda Francisco <francisconehuenmiranda@gmail.com>
2024-05-30 11:33:51 -0300

    EJ4

--- TP3/BuildTensorflowModel.py
+++ TP3/BuildTensorflowModel.py
@@ -106,7 +106,7 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(len(classes), activation='softmax')
 ])
 # ==========================================================================================
@@ -116,8 +116,13 @@ model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
+print("Número total de muestras de entrenamiento:", train_generator.samples)
+print("Número total de muestras de validación:", validation_generator.samples)
 # Entrenar el modelo
 model.fit(train_generator, epochs=10, validation_data=validation_generator)
 
+import os
+
+print("Current Working Directory:", os.getcwd())
 # Guardar el modelo
 model.save('tensorflow_nn.h5')
